2076f027eaa6de411291a33e46abde30ea045d70
Your Name <you@example.com>
2023-05-01 23:40:10 +0700

    реализовал функцию считывания строки

---
+++ src/libspichki/gamelib.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define maxstr 255
+
+struct heap
+{
+    char name;
+    int stock;
+};
+typedef struct heap heap;
+
+struct player
+{
+    char nameOfPile;
+    int countOfItems;
+};
+typedef struct player player;
+
+player* getPlayerPile();
